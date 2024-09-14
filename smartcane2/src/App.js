c237299b607c33c50612afa73cf5b3959bb5eda6
Arav-Mathur <1mathurara@hdsb.ca>
2024-09-14 17:57:06 -0400

    did a bunch of stuff

--- smartcane2/src/App.js
+++ smartcane2/src/App.js
@@ -1,42 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
-import {useState,useEffect} from 'react';
-import * as Location from "expo-location";
+import React, { useState, useEffect, useRef } from 'react';
+import { TGetVenueOptions } from "@mappedin/mappedin-js";
+import "@mappedin/mappedin-js/lib/mappedin.css";
+import useMapView from "./useMapView";
+import useVenue from "./useVenue";
 
-function App() {
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
+const options = { //TODO: talk to Mappedin about this
+  key: "mik_Qar1NBX1qFjtljLDI52a60753",
+  mapId: "66ce20fdf42a3e000b1b0545",
+  secret: "mis_CXFS9WnkQkzQmy9GCt4ucn2D68zNRgVa2aiJj5hEIFM8aa40fee"
+};
 
+export default function App() {
+  const [userLocation, setUserLocation] = useState(null);
   useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        setErrorMsg("Permission to access location was denied");
-        return;
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
+    const watchId = getUserLocation();
+    return () => navigator.geolocation.clearWatch(watchId);
   }, []);
 
-  let text = "Waiting..";
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    let latitude = location.coords.latitude;
-    let longitude = location.coords.longitude;
-    text = JSON.stringify({latitude, longitude});
+  const getUserLocation = () => {
+    if(navigator.geolocation){
+      const watch = navigator.geolocation.watchPosition((position) => {
+        const {latitude, longitude} = position.coords;
+        setUserLocation({latitude,longitude});
+      },
+      (error)=>{
+        console.error('error getting location: ',error)
+      }
+    );
+    return watch;
+    }
+    else console.error('Geolocation is not supported by this browser');
   }
+
+  //mappedin
+  const mapRef = useRef(null);
+  const venue = useVenue(options);
+  const mapView = useMapView(mapRef.current, venue);
+
+  async function getDirections(end="E7 Main Entrance"){
+    const startLocation = venue.locations.find((location) => location.coords === userLocation);
+  
+    //Find the location with the name "The Body Shop" to use as an end point.
+    const endLocation = venue.locations.find((location) => location.name === "The Body Shop");
+  
+    //Get directions between the start and end locations.
+    const directions = startLocation?.directionsTo(endLocation);
+  
+    console.log(directions.instructions[0]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
         <p>Welcome to smart cane</p>
-      </header>
-      <p>{text}</p>
-
+      <div className='App-link'>
+        {/*userLocation ? 
+        <div>
+        <p>Latitude: {JSON.stringify(userLocation.latitude)}</p>
+        <p>Longitude: {JSON.stringify(userLocation.longitude)}</p>
+        </div>
+        : <p>Getting the location data</p>*/}
+      </div>
+      <div style={{'background-color':'#000000','border':'10px'}}><div  id="app" ref={mapRef} /></div>
+      <button onClick={()=>getDirections()}>get directions</button>
+      
     </div>
   );
 }
-
-export default App;
